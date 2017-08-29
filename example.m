afe97cfe4705aecde97be2612734272daf24043a
Brad Lipovsky <bradlipovsky@gmail.com>
2017-08-29 10:06:32 -0400

    Added an example

---
+++ example.m
@@ -0,0 +1,22 @@
+clear;
+
+%
+% This example re-creates Figure 8a from the Lipovsky and Dunham (2017).
+%
+% Please give credit to this work by citing:
+%
+% Lipovsky, Bradley Paul, and Eric M. Dunham. "Slow‐slip events on the 
+% 	Whillans Ice Plain, Antarctica, described using rate‐and‐state friction 
+%	as an ice stream sliding law." Journal of Geophysical Research: Earth 
+%	Surface 122.4 (2017): 973-1003. http://dx.doi.org/10.1002/2016JF004183
+%
+
+
+main( 8.1,14,5,15 );
+
+%
+% Usage notes are in the file main.m
+%
+% Note that this takes about 30 minutes to run on my 2016 Mac Pro with 
+% 64 Gb memory and 8 cores.
+%
